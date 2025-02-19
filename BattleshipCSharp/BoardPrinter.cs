c65acb60b64982711112e62ca01ea0f4f56e74b8
David Browning <david.a.browning@gmail.com>
2025-02-19 13:11:08 +0100

    Tidy up if-else chain.

--- BattleshipCSharp/BoardPrinter.cs
+++ BattleshipCSharp/BoardPrinter.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices.Marshalling;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,7 +10,14 @@ namespace BattleshipCSharp
 {
     internal static class BoardPrinter
     {
+
         public static void Print(Board board)
+        {
+            PrintColumnHeaders(board);
+            PrintBoardArea(board);
+            PrintColumnHeaders(board);
+        }
+        private static void PrintColumnHeaders(Board board)
         {
             TextPrinter.PrintHeaderTile(" ");
             for (int x = board.XMin; x <= board.XMax; x++)
@@ -17,44 +25,33 @@ namespace BattleshipCSharp
                 TextPrinter.PrintHeaderTile(x.ToString());
             }
             Console.WriteLine();
+        }
+        private static void PrintBoardArea(Board board)
+        {
             for (int y = board.YMin; y <= board.YMax; y++)
             {
-                TextPrinter.PrintHeaderTile($"{(char)(65 + y)}");
+                PrintRowHeader(y);
                 for (int x = board.XMin; x <= board.XMax; x++)
                 {
                     Location location = new Location(x, y);
                     if (board.Fleet.IsSunk(location))
-                    {
                         TextPrinter.PrintSunkTile();
-                    }
                     else if (board.Fleet.IsHit(location))
-                    {
                         TextPrinter.PrintHitTile();
-                    }
                     else if (board.Fleet.Contains(location))
-                    {
                         TextPrinter.PrintEmptyTile();
-                    }
                     else if (board.Attempts.Contains(location))
-                    {
                         TextPrinter.PrintMissTile();
-                    }
                     else
-                    {
                         TextPrinter.PrintEmptyTile();
-                    }
                 }
-                TextPrinter.PrintHeaderTile($"{(char)(65 + y)}");
+                PrintRowHeader(y);
                 Console.WriteLine();
             }
-
-            TextPrinter.PrintHeaderTile(" ");
-            for (int x = board.XMin; x <= board.XMax; x++)
-            {
-                TextPrinter.PrintHeaderTile(x.ToString());
-            }
-
-            Console.WriteLine();
+        }
+        private static void PrintRowHeader(int y)
+        {
+            TextPrinter.PrintHeaderTile($"{(char)(65 + y)}");
         }
     }
 }
